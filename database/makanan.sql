6db0efd0f4a4e4e852aeb8d266bf78c2d311ba3a
NaTa64 <fachrihardinata64@gmail.com>
2025-04-14 22:46:20 +0800

    database_baru

--- database/makanan.sql
+++ database/makanan.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 23, 2025 at 06:40 AM
+-- Generation Time: Apr 14, 2025 at 03:38 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -84,10 +84,7 @@ CREATE TABLE `cart` (
 CREATE TABLE `customers` (
   `cust_id` int(11) NOT NULL,
   `cust_ip` varchar(225) NOT NULL,
-  `cust_fname` text DEFAULT NULL,
-  `cust_lname` text DEFAULT NULL,
   `cust_dname` text NOT NULL,
-  `cust_email` varchar(225) DEFAULT NULL,
   `username` varchar(100) NOT NULL,
   `cust_pass` varchar(225) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -96,8 +93,9 @@ CREATE TABLE `customers` (
 -- Dumping data for table `customers`
 --
 
-INSERT INTO `customers` (`cust_id`, `cust_ip`, `cust_fname`, `cust_lname`, `cust_dname`, `cust_email`, `username`, `cust_pass`) VALUES
-(1, '::1', NULL, NULL, 'Rengga', NULL, 'rengga', '123');
+INSERT INTO `customers` (`cust_id`, `cust_ip`, `cust_dname`, `username`, `cust_pass`) VALUES
+(1, '::1', 'Rengga', 'rengga', '123'),
+(2, '::1', 'fachri', 'fachri', '1');
 
 -- --------------------------------------------------------
 
@@ -108,7 +106,6 @@ INSERT INTO `customers` (`cust_id`, `cust_ip`, `cust_fname`, `cust_lname`, `cust
 CREATE TABLE `items` (
   `item_id` int(11) NOT NULL,
   `item_name` varchar(225) NOT NULL,
-  `item_desc` varchar(225) DEFAULT NULL,
   `stok` int(11) NOT NULL,
   `harga` decimal(6,3) NOT NULL,
   `item_image` varchar(100) DEFAULT NULL,
@@ -120,19 +117,20 @@ CREATE TABLE `items` (
 -- Dumping data for table `items`
 --
 
-INSERT INTO `items` (`item_id`, `item_name`, `item_desc`, `stok`, `harga`, `item_image`, `item_keywords`, `aktif`) VALUES
-(1, 'Pisang Keju', 'CHICK-FIL-A', 10, 10.000, 'Pictures/1.jpg', 'keju', 1),
-(2, 'Double-Double', 'IN-N-OUT', 97, 4.040, 'Pictures/2.jpg', 'DOUBLE-DOUBLE,Chicken,Sandwich', 1),
-(3, 'Fries', 'MCDONALD\'S', 95, 15.000, 'Pictures/3.jpg', 'Fries, mcdonald,snacks', 1),
-(4, 'Chicken', 'POPEYES', 440, 6.770, 'Pictures/4.jpg', 'Chicken,Nuggets', 1),
-(5, 'Chicken Sandwich', 'CHICK-FIL-A', 10, 3.900, 'Pictures/5.jpg', 'Chicken,Sandwich', 1),
-(6, 'Curly Fries', 'ARBY\'S', 400, 2.550, 'Pictures/6.jpg', 'Curly,Fries,snacks', 1),
-(7, 'Blizzard', 'DAIRY QUEEN', 620, 5.240, 'Pictures/7.jpg', 'Blizzard,Ice cream,desserts', 1),
-(8, 'Frosty', 'WENDY\'S', 393, 2.550, 'Pictures/8.jpg', 'Frosty', 1),
-(9, 'Mcflurry', 'MCDONALD\'S', 520, 2.110, 'Pictures/9.jpg', 'Ice cream,Mcflurry', 1),
-(10, 'a', 'FIVE GUYS', 11, 11.011, 'Pictures/10.jpg', 'Bacon,Cheese,Burger', 1),
-(11, 'Ayam', NULL, 10, 200.000, 'Pictures/20230417_185527.jpeg', NULL, 0),
-(12, 'sapi', NULL, 1, 100.000, 'Pictures/Figure_1.png', NULL, 0);
+INSERT INTO `items` (`item_id`, `item_name`, `stok`, `harga`, `item_image`, `item_keywords`, `aktif`) VALUES
+(1, 'Pisang Keju', 64, 10.000, 'Pictures/1.jpg', 'keju', 1),
+(2, 'Double-Double', 62, 4.000, 'Pictures/2.jpg', 'DOUBLE-DOUBLE,Chicken,Sandwich', 1),
+(3, 'Fries', 50, 15.000, 'Pictures/3.jpg', 'Fries, mcdonald,snacks', 1),
+(4, 'Chicken', 434, 5.000, 'Pictures/4.jpg', 'Chicken,Nuggets', 1),
+(5, 'Chicken Sandwich', 3, 3.900, 'Pictures/5.jpg', 'Chicken,Sandwich', 1),
+(6, 'Curly Fries', 397, 2.550, 'Pictures/6.jpg', 'Curly,Fries,snacks', 1),
+(7, 'Blizzard', 620, 5.240, 'Pictures/7.jpg', 'Blizzard,Ice cream,desserts', 1),
+(8, 'Frosty', 392, 2.550, 'Pictures/8.jpg', 'Frosty', 1),
+(9, 'Mcflurry', 520, 2.110, 'Pictures/9.jpg', 'Ice cream,Mcflurry', 1),
+(10, 'a', 8, 11.011, 'Pictures/10.jpg', 'Bacon,Cheese,Burger', 1),
+(11, 'Ayam', 10, 200.000, 'Pictures/20230417_185527.jpeg', NULL, 0),
+(12, 'sapi', 1, 100.000, 'Pictures/Figure_1.png', NULL, 0),
+(13, 'bayi muffin', 80, 10.000, 'Pictures/Jake_Portrait_Render.webp', NULL, 1);
 
 -- --------------------------------------------------------
 
@@ -144,15 +142,15 @@ CREATE TABLE `orders` (
   `order_id` int(11) NOT NULL,
   `cust_id` int(11) NOT NULL,
   `cust_ip` varchar(225) NOT NULL,
-  `order_amount` decimal(6,3) DEFAULT NULL,
-  `jumlah` int(11) DEFAULT NULL,
+  `total` decimal(6,3) DEFAULT NULL,
   `name` varchar(50) NOT NULL,
   `alamat` varchar(100) NOT NULL,
   `phone` varchar(15) DEFAULT NULL,
   `delivery_type` text NOT NULL,
   `payment` text NOT NULL,
-  `status` text NOT NULL,
+  `status` varchar(20) NOT NULL,
   `tanggal_order` datetime DEFAULT NULL,
+  `tanggal_selesai` datetime DEFAULT NULL,
   `aktif` tinyint(1) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -160,8 +158,10 @@ CREATE TABLE `orders` (
 -- Dumping data for table `orders`
 --
 
-INSERT INTO `orders` (`order_id`, `cust_id`, `cust_ip`, `order_amount`, `jumlah`, `name`, `alamat`, `phone`, `delivery_type`, `payment`, `status`, `tanggal_order`, `aktif`) VALUES
-(2, 1, '::1', 36.040, 3, 'Muhammad Fachri', 'Jl.SoekarnoHatta Km 1', '0895700288991', 'Home Delivery', 'Cash on Delivery', 'Sedang Diproses', '2025-03-23 13:19:37', 1);
+INSERT INTO `orders` (`order_id`, `cust_id`, `cust_ip`, `total`, `name`, `alamat`, `phone`, `delivery_type`, `payment`, `status`, `tanggal_order`, `tanggal_selesai`, `aktif`) VALUES
+(1, 2, '::1', 27.000, 'Muhammad Fachri', 'Jl.SoekarnoHatta Km 1', '0895700288991', 'Home Delivery', 'Cash on Delivery', 'Sedang Diantar', '2025-03-26 13:48:03', NULL, 1),
+(2, 1, '125.160.113.77', 29.461, 'bebelak', 'jalan suci', '080808080808', 'Home Delivery', 'Cash on Delivery', 'Telah Selesai', '2025-03-26 13:47:55', '2025-04-05 14:35:44', 0),
+(3, 2, '::1', 17.000, 'Muhammad Fachriaw', 'Jl.SoekarnoHatta Km 1', '0895700288991', '', '', 'Telah Selesai', '2025-04-05 20:36:45', '2025-04-05 20:46:49', 0);
 
 -- --------------------------------------------------------
 
@@ -170,22 +170,26 @@ INSERT INTO `orders` (`order_id`, `cust_id`, `cust_ip`, `order_amount`, `jumlah`
 --
 
 CREATE TABLE `order_items` (
-  `cust_id` int(11) NOT NULL,
+  `order_id` int(11) NOT NULL,
   `username` varchar(100) NOT NULL,
-  `i_id` int(11) NOT NULL,
   `ip_add` varchar(225) NOT NULL,
-  `qty` int(10) NOT NULL
+  `item_id` int(11) NOT NULL,
+  `qty` int(10) NOT NULL,
+  `subtotal` decimal(6,3) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `order_items`
 --
 
-INSERT INTO `order_items` (`cust_id`, `username`, `i_id`, `ip_add`, `qty`) VALUES
-(1, 'rengga', 3, '::1', 1),
-(1, 'rengga', 2, '::1', 1),
-(1, 'rengga', 3, '::1', 1),
-(1, 'rengga', 2, '::1', 1);
+INSERT INTO `order_items` (`order_id`, `username`, `ip_add`, `item_id`, `qty`, `subtotal`) VALUES
+(2, 'rengga', '125.160.113.77', 5, 1, 3.900),
+(2, 'rengga', '125.160.113.77', 1, 1, 10.000),
+(2, 'rengga', '125.160.113.77', 8, 1, 2.550),
+(2, 'rengga', '125.160.113.77', 10, 1, 11.011),
+(1, 'fachri', '::1', 3, 1, 15.000),
+(1, 'fachri', '::1', 1, 1, 10.000),
+(3, 'fachri', '::1', 3, 1, 15.000);
 
 --
 -- Indexes for dumped tables
@@ -232,8 +236,7 @@ ALTER TABLE `orders`
 -- Indexes for table `order_items`
 --
 ALTER TABLE `order_items`
-  ADD KEY `i_id` (`i_id`),
-  ADD KEY `cust_id` (`cust_id`);
+  ADD KEY `item_id` (`item_id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -249,19 +252,19 @@ ALTER TABLE `admin`
 -- AUTO_INCREMENT for table `customers`
 --
 ALTER TABLE `customers`
-  MODIFY `cust_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `cust_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `items`
 --
 ALTER TABLE `items`
-  MODIFY `item_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `item_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT for table `orders`
 --
 ALTER TABLE `orders`
-  MODIFY `order_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `order_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- Constraints for dumped tables
@@ -283,8 +286,7 @@ ALTER TABLE `orders`
 -- Constraints for table `order_items`
 --
 ALTER TABLE `order_items`
-  ADD CONSTRAINT `order_items_ibfk_1` FOREIGN KEY (`i_id`) REFERENCES `items` (`item_id`) ON DELETE CASCADE,
-  ADD CONSTRAINT `order_items_ibfk_2` FOREIGN KEY (`cust_id`) REFERENCES `customers` (`cust_id`) ON DELETE CASCADE;
+  ADD CONSTRAINT `item_id` FOREIGN KEY (`item_id`) REFERENCES `items` (`item_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
