68ea88c73b2f48306b605ca639a1ef5964a1ebf3
NaTa64 <fachrihardinata64@gmail.com>
2025-06-19 23:16:26 +0800

    Betulan Final 2

--- checkout4.php
+++ checkout4.php
@@ -257,9 +257,8 @@ if (isset($_GET['od_id'])) {
             $conn->exec($insert_temp);
         }
 
-        $mtotal=0;
         // Query untuk mengupdate orderan sekaligus menambah orderan
-        $update_c = "update orders set status='Sedang Diproses', aktif=1, total ='" . $mtotal . "', tanggal_order=DATE_FORMAT(NOW(), '%Y-%m-%d %H:%i:%s') where order_id='" . $or_id . "'";
+        $update_c = "update orders set status='Sedang Diproses', aktif=1, total ='" . ($final_total + $ongkir) . "', tanggal_order=DATE_FORMAT(NOW(), '%Y-%m-%d %H:%i:%s') where order_id='" . $or_id . "'";
         $del_cart = $conn->query("delete from cart where cust_id = '" . $cust_id . "'");
         if ($conn->query($update_c) == TRUE) {
             echo "<script>window.open('ordersucces.php?cancel_id=1','_self')</script>";
